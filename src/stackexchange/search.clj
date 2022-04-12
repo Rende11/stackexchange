82928e9a9392477574690a9a9261f777e1e80b06
Rende11 <artamonovn@gmail.com>
2022-04-12 21:19:33 +0300

    Naive sequential search.

--- src/stackexchange/search.clj
+++ src/stackexchange/search.clj
@@ -1,10 +1,33 @@
 (ns stackexchange.search
   (:require [clj-http.client :as client]))
 
+(defn stackexchange-search [tag]
+  (let [resp (try (client/get "https://api.stackexchange.com/2.3/search"
+                              {:query-params {:page-size 100
+                                              :order "desc"
+                                              :sort "creation"
+                                              :tagged tag
+                                              :site "stackoverflow"}
+                               :as :json})
+                  (catch Exception e
+                    (throw (ex-info (ex-message e)
+                                    {:message "Stackexchange search failed"
+                                     :data {:tag tag}}
+                                    e))))
+        items (-> resp :body :items)
+        answered (filter :is_answered items)
+        answer-count-sum (->> answered
+                              (map :answer_count)
+                              (apply +))]
+    {:total (count answered)
+     :answered answer-count-sum}))
+
+
+
 (defn search [{:keys [params] :as req}]
-  {:status 200
-   :body {:message "My search"
-          :params params}})
-
-
-
+  (let [stats (reduce (fn [acc tag]
+                        (assoc acc tag (stackexchange-search tag)))
+                      {}
+                      (:tag params))]
+    {:status 200
+     :body stats}))
